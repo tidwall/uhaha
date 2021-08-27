98e54a7c81f7d194099763cf83a4da00fdf33c87
Jchicode <qianggeek@gmail.com>
2021-08-27 16:22:15 +0800

    fix(cmdRewrite):remove cmdRewrite method

--- examples/kvdb/main.go
+++ examples/kvdb/main.go
@@ -31,7 +31,6 @@ func main() {
 	conf.Snapshot = snapshot
 	conf.Restore = restore
 	conf.Tick = tick
-	conf.CmdRewriteFunc = cmdRewrite
 
 	conf.AddWriteCommand("set", cmdSET)
 	conf.AddWriteCommand("del", cmdDEL)
@@ -407,44 +406,6 @@ func restore(rd io.Reader) (interface{}, error) {
 		db.set(o)
 	}
 	return db, nil
-}
-
-func cmdRewrite(args [][]string) {
-	nowUnixTime := time.Now().Unix()
-
-	for i, arg := range args {
-		switch strings.ToLower(arg[0]) {
-		case "setex":
-			if len(arg) == 4 {
-				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
-				if err == nil {
-					args[i] = []string{"setexat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10), arg[3]}
-				}
-			}
-		case "expire":
-			if len(arg) == 3 {
-				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
-				if err == nil {
-					args[i] = []string{"expireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10)}
-				}
-			}
-		case "lexpire":
-			if len(arg) == 3 {
-				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
-				if err == nil {
-					args[i] = []string{"lexpireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10)}
-				}
-			}
-		case "hexpire":
-			if len(arg) == 3 {
-				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
-				if err == nil {
-					args[i] = []string{"lexpireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10)}
-				}
-			}
-		default:
-		}
-	}
 }
 
 // #endregion -- SNAPSHOT & RESTORE
