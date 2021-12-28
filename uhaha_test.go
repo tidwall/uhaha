a30a13e5bc517fe2f22aa572ad0633aaafd44263
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2021-12-28 16:34:09 +0900

    auto eject test

--- uhaha_test.go
+++ uhaha_test.go
@@ -2,6 +2,7 @@ package uhaha
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -416,5 +417,202 @@ func TestLeaderAdvertise(t *testing.T) {
 		[]int{1, 3, 5}, // sizes
 		50,             // clients
 		func() TestClusterContext { return newLeaderAdvertiseTestCluster() },
+	)
+}
+
+// AUTO EJECT TEST
+
+const autoeject_app string = `
+package main
+
+import (
+	"github.com/tidwall/uhaha"
+	"time"
+)
+
+func main() {
+	var conf uhaha.Config
+	conf.Name = "autoejectapp"
+	conf.InitialData = struct{}{}
+	conf.AutoEjectServers = true
+	conf.ServerFailureLimit = 3
+	conf.ServerContactTimeout = 10 * time.Second
+	uhaha.Main(conf)
+}
+`
+
+func buildAutoEjectEnabledApp(t *testing.T) {
+	temp, err := ioutil.TempFile("", "testautoejectapp*.go")
+	if err != nil {
+		wlog("::BUILD::FAIL::\n%s\n", err.Error())
+		badnews()
+	}
+	if _, err := temp.WriteString(autoeject_app); err != nil {
+		wlog("::BUILD::FAIL::\n%s\n", err.Error())
+		badnews()
+	}
+	temp.Close()
+
+	t.Cleanup(func() {
+		os.Remove(temp.Name())
+	})
+
+	run("go", "build", "-o", filepath.Join("testing", app), temp.Name())
+
+	wlog("::BUILD::Ok\n")
+}
+
+func testAutoEjectClusters(t *testing.T, sizes []int, numClients int,
+	newCtx func() TestClusterContext,
+) {
+	genSeed()
+	must(nil, os.MkdirAll("testing", 0777))
+	verifyGoVersion()
+	buildAutoEjectEnabledApp(t)
+	for _, size := range sizes {
+		t.Run(fmt.Sprintf("%d", size), func(t *testing.T) {
+			testSingleCluster(size, newCtx(), numClients)
+		})
+	}
+}
+
+type autoEjectTestCluster struct{}
+
+func newAutoEjectTestCluster() *autoEjectTestCluster {
+	return &autoEjectTestCluster{}
+}
+
+func (ctx *autoEjectTestCluster) Start(size int, numClients int) {
+}
+
+func (ctx *autoEjectTestCluster) Monitor(size int) {
+	conns := make([]redis.Conn, size)
+	for i := 0; i < size; i += 1 {
+		conn, err := redis.Dial("tcp", fmt.Sprintf(":3300%d", i+1))
+		if err != nil {
+			wlog("::CLIENT::Error %s", err.Error())
+			badnews()
+		}
+		conns[i] = conn
+	}
+	defer func() {
+		for _, conn := range conns {
+			conn.Close()
+		}
+	}()
+
+	for _, conn := range conns {
+		reply, err := redis.String(conn.Do("PING"))
+		if err != nil {
+			wlog("::CLIENT::%s", err)
+			badnews()
+		}
+		if reply != "PONG" {
+			wlog("::CLIENT::Expected 'PONG' got '%s'", reply)
+			badnews()
+		}
+	}
+
+	getServerAddresses := func() string {
+		serverlist := ""
+		for _, conn := range conns {
+			reply, err := conn.Do("RAFT", "SERVER", "LIST")
+			if err != nil {
+				wlog("::CLIENT::%s", err)
+				badnews()
+			}
+			addresses := make([]string, 0)
+			replies := reply.([]interface{})
+			for _, r := range replies {
+				s, _ := redis.Strings(r, nil)
+				for i := 0; i < len(s); i += 2 {
+					key, value := s[i], s[i+1]
+					if strings.ToLower(key) == "address" {
+						addresses = append(addresses, value)
+					}
+				}
+			}
+			curr := strings.Join(addresses, ",")
+
+			if serverlist == "" {
+				serverlist = curr
+			}
+			if serverlist != curr {
+				wlog(
+					"::CLIENT::RAFT_SERVER_LIST not same %s <> %s",
+					serverlist,
+					curr,
+				)
+				badnews()
+			}
+		}
+		return serverlist
+	}
+	grepConn := func(index int) []redis.Conn {
+		newConns := make([]redis.Conn, 0, len(conns)-1)
+		for i, conn := range conns {
+			if i != index {
+				newConns = append(newConns, conn)
+			}
+		}
+		return newConns
+	}
+
+	a1 := getServerAddresses()
+
+	// first node shutdown
+	r1 := rand.Int() % size
+	wlog("::SERVER::shutdown %d", r1)
+	if _, err := conns[r1].Do("shutdown"); err != nil {
+		if errors.Is(err, io.EOF) != true {
+			wlog("::CLIENT::%s", err)
+			badnews()
+		}
+	}
+	conns = grepConn(r1)
+
+	// wait ServerContactTimeout
+	time.Sleep(time.Second * 15)
+
+	a2 := getServerAddresses()
+
+	if a1 == a2 {
+		wlog("::SERVER::auto_eject failed same servers %s == %s", a1, a2)
+		badnews()
+	}
+	wlog("::SERVER list=%s", a2)
+
+	if 2 < (size - 1) {
+		// second node shutdown
+		r2 := rand.Int() % (size - 1)
+		wlog("::SERVER::shutdown %d", r2)
+		if _, err := conns[r2].Do("shutdown"); err != nil {
+			if errors.Is(err, io.EOF) != true {
+				wlog("::CLIENT::%s", err)
+				badnews()
+			}
+		}
+		conns = grepConn(r2)
+
+		// wait ServerContactTimeout
+		time.Sleep(time.Second * 15)
+
+		a3 := getServerAddresses()
+
+		if a2 == a3 {
+			wlog("::SERVER::auto_eject failed same servers %s == %s", a1, a2)
+			badnews()
+		}
+		wlog("::SERVER list=%s", a3)
+	}
+}
+
+func (ctx *autoEjectTestCluster) ExecClient(size int, n int, c *TestConn) {}
+
+func TestAutoEject(t *testing.T) {
+	testAutoEjectClusters(t,
+		[]int{3, 5},
+		10,
+		func() TestClusterContext { return newAutoEjectTestCluster() },
 	)
 }
